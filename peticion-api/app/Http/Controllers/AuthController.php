b1d99116a7c342bc17ff0a97604376f098c23098
Jordy Adonay Mejía Castellano <st2021024@365.stucom.com>
2023-05-16 19:09:45 +0200

    updates

--- peticion-api/app/Http/Controllers/AuthController.php
+++ peticion-api/app/Http/Controllers/AuthController.php
@@ -1,10 +1,48 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function signup(Request $request){
+    $user = User::create([
+    'name' => $request->input('name'),
+    'email' => $request->input('email'),
+    'password' => bcrypt($request->input('password')),
+    ]);
+    return response()->json(['message' => 'User signed up successfully'], 201);
+    }
+    public function login(Request $request){
+            $validator = Validator::make($request->all(), [
+            'email' => 'required | email',
+            'password' => 'required',
+            ]);
+            
+            if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400);
+            }
+            
+            $user = User::where('email', $request->input('email'))->first();
+            
+            if (!$user) {
+            return response()->json(['error' => 'Email is not registered'], 404);
+            }
+    
+            if (!password_verify($request->input('password'), $user->password)) {
+                return response()->json(['error' => 'Incorrect password'], 400);
+            }
+            
+            $payload = [
+            'iss' => config('app.url'),
+            'sub' => $user->id,
+            'iat' => time(),
+            'exp' => time() + (60 * 60 * 24),
+            ];
+            $jwt = JWT::encode($payload, config('app.key'), 'HS256');
+            return response()->json(['token' => $jwt]);
+    }
+    public function logout(Request $request){
+    }
 }
--- peticion-api/routes/api.php
+++ peticion-api/routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,7 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::post('/signup', [AuthController::class, 'signup'])->name('signup');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/* Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+}); */
+
